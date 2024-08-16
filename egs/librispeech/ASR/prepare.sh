93af999635486934e1871b8dd0596b4b9868f076
marcoyang <marcoyang1998@gmail.com>
2024-08-16 17:41:29 +0800

    make speed perturb to False by default

--- egs/librispeech/ASR/prepare.sh
+++ egs/librispeech/ASR/prepare.sh
@@ -119,7 +119,7 @@ if [ $stage -le 3 ] && [ $stop_stage -ge 3 ]; then
   log "Stage 3: Compute fbank for librispeech"
   mkdir -p data/fbank
   if [ ! -e data/fbank/.librispeech.done ]; then
-    ./local/compute_fbank_librispeech.py
+    ./local/compute_fbank_librispeech.py --perturb-speed False \
     touch data/fbank/.librispeech.done
   fi
 
