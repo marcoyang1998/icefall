37964422c3dc9c9d28a36281a9f268efa47065ce
X. Yang <xy316@cam.ac.uk>
2024-11-09 21:41:20 +0000

    add script to collect dasheng embedding

---
+++ egs/audioset/AT/local/collect_dasheng_embeddings.py
@@ -0,0 +1,155 @@
+import logging
+import argparse
+
+import torch
+
+from lhotse import load_manifest_lazy, CutSet
+from lhotse.features.io import LilcomChunkyWriter
+from lhotse.utils import fastcopy
+from torch.utils.data import DataLoader
+from lhotse.dataset import DynamicBucketingSampler, UnsupervisedWaveformDataset
+
+from dasheng import dasheng_base, dasheng_06B, dasheng_12B
+
+MODEL_DICT = {
+    "base": dasheng_base,
+    "medium": dasheng_06B,
+    "large": dasheng_12B,
+}
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    
+    parser.add_argument(
+        "--dasheng-version",
+        type=str,
+        required=True,
+        choices=["base", "medium", "large"]
+    )
+    
+    parser.add_argument(
+        "--layer-idx",
+        type=int,
+        default=-1,
+        help="""Index starts from 1, to get the 12-th layer features, set layer-idx=12. If 
+        layer-idx is set to -1, then use the final output feature (after final norm).
+        """
+    )
+    
+    parser.add_argument(
+        "--subset",
+        type=str,
+        required=True,
+    )
+    
+    return parser.parse_args()
+
+def test_wavlm():
+    import torchaudio
+    model = dasheng_base()
+
+    model = model.eval()
+
+    wav = "download/audioset/balanced/ZzyWbehtt0M_30.000.wav"
+    audio, fs = torchaudio.load(wav)
+
+    with torch.no_grad():
+        import pdb; pdb.set_trace()
+        features, all_hidden_states = model(audio, output_hidden_states=True) # 25 Hz output, 
+
+    import pdb; pdb.set_trace()
+    print(features.shape) #(B,T,C)
+    
+@torch.no_grad()
+def collect_results(dasheng_version, manifest_path, embedding_path, output_manifest_path, layer_idx=21, max_duration=200):
+    
+    # load the pretrained dasheng model
+    model = MODEL_DICT[dasheng_version]()
+    model.eval()
+    
+    manifest = load_manifest_lazy(manifest_path)
+    dataset = UnsupervisedWaveformDataset(
+        manifest
+    )
+    
+    sampler = DynamicBucketingSampler(
+        manifest,
+        max_duration=max_duration,
+        shuffle=False,
+        drop_last=False,
+    )
+    
+    dl = DataLoader(
+        dataset,
+        sampler=sampler,
+        batch_size=None,
+        num_workers=1,
+        persistent_workers=False,
+    )
+    
+    device = torch.device("cuda")
+    model.to(device)
+    
+    new_cuts = []
+    num_cuts = 0
+    with LilcomChunkyWriter(embedding_path) as writer:
+        for i, batch in enumerate(dl):
+            cuts = batch["cuts"]
+            audio_input_16khz = batch["audio"].to(device)
+            audio_lens = batch["audio_lens"].to(device)
+            
+            features, all_hidden_states = model(audio_input_16khz, output_hidden_states=True)
+            
+            if layer_idx != -1:
+                layer_results = all_hidden_states[layer_idx] # (B,T,C)
+            else:
+                layer_results = features # (B,T,C)
+            layer_results = layer_results.cpu().numpy()
+            embedding_lens = audio_lens // 640 # The output frequency is 25Hz
+            
+            for j, cut in enumerate(cuts):
+                embeddings = writer.store_array(
+                    key=cut.id,
+                    value=layer_results[j][:embedding_lens[j]],
+                    temporal_dim=0,
+                    frame_shift=0.02,
+                    start=0,
+                )
+                new_cut = fastcopy(
+                    cut,
+                    custom={"dasheng_embedding": embeddings}
+                )
+                new_cuts.append(new_cut)
+                num_cuts += 1
+                if num_cuts and num_cuts % 200 == 0:
+                    logging.info(f"Cuts processed until now: {num_cuts}")
+    
+    new_cuts = CutSet.from_cuts(new_cuts)
+    new_cuts.to_jsonl(output_manifest_path)
+    logging.info(f"Manifest saved to {output_manifest_path}")
+            
+        
+if __name__=="__main__":
+    formatter = "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
+    logging.basicConfig(format=formatter, level=logging.INFO)
+    
+    args = get_parser()
+    dasheng_version = args.dasheng_version
+    layer_idx = args.layer_idx
+    subset = args.subset
+    
+    manifest_path = f"data/fbank_audioset/cuts_audioset_{subset}.jsonl.gz"
+    embedding_path = f"embeddings/dasheng_embeddings/dasheng-{dasheng_version}-layer-{layer_idx}-{subset}.h5"
+    output_manifest_path = f"manifests/{subset}-dasheng-{dasheng_version}-layer-{layer_idx}.jsonl.gz"
+    
+    max_duration = 200
+    collect_results(
+        dasheng_version=dasheng_version,
+        manifest_path=manifest_path,
+        embedding_path=embedding_path,
+        output_manifest_path=output_manifest_path,
+        layer_idx=layer_idx,
+        max_duration=max_duration,
+    )
