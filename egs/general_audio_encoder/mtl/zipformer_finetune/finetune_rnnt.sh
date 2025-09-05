57301b751ef5b5c4116128d312aa4f74310a21f7
marco <xy316@cam.ac.uk>
2025-09-06 07:11:59 +0800

    add example finetune script

--- egs/general_audio_encoder/mtl/zipformer_finetune/finetune_rnnt.sh
+++ egs/general_audio_encoder/mtl/zipformer_finetune/finetune_rnnt.sh
@@ -13,6 +13,8 @@ lr=0.045
 do_finetune=1
 finetune_ckpt=zipformer_audio_encoder/exp-316M-uniform-v2-out-ds-1-zipformer-lh-large-giga-xl-emo-0-voxpopuli-1-lr-batches-7500-lr-hours-75000-w2v2-mask-p-0.5-l-10-cha-mask-p-0.25-l-15-batch-mix-p-0.3-min-snr--5-p-noise-0.7-min-snr-5-wavlm-large-layer-21-libri-mvq-cb-16-shar-md-400-16-gpus/iter-500000-avg-4.pt
 
+use_ctc=0
+use_transducer=1
 output_ds=1
 post_output_ds=1
 
@@ -24,11 +26,11 @@ encoder_lr_scale=0.05
 
 md=1000
 
-exp_dir=zipformer_finetune/exp-finetune-ctc
+exp_dir=zipformer_finetune/exp-finetune-rnnt
 
 echo $exp_dir
 
-torchrun --nproc_per_node=2 --master_port=19290 \
+torchrun --nproc_per_node=2 --master_port=19291 \
   zipformer_finetune/finetune_asr.py \
     --num-epochs 30 \
     --use-fp16 1 \
@@ -36,8 +38,9 @@ torchrun --nproc_per_node=2 --master_port=19290 \
     --use-librispeech $use_librispeech --full-libri $full_libri \
     --exp-dir $exp_dir \
     --manifest-dir data/fbank \
-    --lang-dir k2ssl-asr-librispeech-100h-zipformer-base-ctc/data/lang_char \
+    --bpe-model data/lang_bpe_500/bpe.model \
     --base-lr $lr \
+    --use-ctc $use_ctc --use-transducer $use_transducer \
     --do-finetune $do_finetune --init-modules "encoder_embed,encoder" --finetune-ckpt $finetune_ckpt \
     --freeze-encoder $freeze_encoder --freeze-encoder-steps $freeze_encoder_steps \
     --encoder-lr-scale $encoder_lr_scale \
@@ -54,14 +57,14 @@ torchrun --nproc_per_node=2 --master_port=19290 \
     --on-the-fly-feats 1 \
     --max-duration $md
 
-for m in ctc-decoding; do
+for m in greedy_search modified_beam_search; do
     for epoch in 30; do
-        for avg in $(seq 18 -1 13); do
-            python zipformer_finetune/decode_ctc.py \
+        for avg in $(seq 15 -1 10); do
+            python zipformer_finetune/decode.py \
                 --epoch $epoch \
                 --avg $avg \
                 --manifest-dir data/fbank_librispeech \
-                --lang-dir k2ssl-asr-librispeech-100h-zipformer-base-ctc/data/lang_char \
+                --bpe-model data/lang_bpe_500/bpe.model \
                 --use-averaged-model 1 \
                 --downsampling-factor 1,2,4,8,4,2,1 \
                 --num-encoder-layers 1,2,2,3,1,1,1 \
